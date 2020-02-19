8b0d1e12cfb4d04832f8810c05c66dc51ccc793b
laciz <52106045+laciz@users.noreply.github.com>
2020-02-19 20:07:56 +0100

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,23 +1,23 @@
 <?php
 include 'functions/db_config.php';
 if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
-    // Verify data
-    $email = mysqli_escape_string($con,$_GET['email']); // Set email variable
-    $hash = mysqli_escape_string($con,$_GET['hash']); // Set hash variable
+   
+    $email = mysqli_escape_string($con,$_GET['email']); /
+    $hash = mysqli_escape_string($con,$_GET['hash']); 
 
     $search = mysqli_query($con,"SELECT email, hash, active FROM users WHERE email='".$email."' AND hash='".$hash."' AND active='0'") or die(mysqli_error());
     $match  = mysqli_num_rows($search);
 
     if($match > 0){
-        // We have a match, activate the account
+      
         mysqli_query($con,"UPDATE users SET active='1' WHERE email='".$email."' AND hash='".$hash."' AND active='0'") or die(mysqli_error());
         echo '<div class="statusmsg">Sikeresen aktiváltad fiókod,mostmár betudsz jelentkezni.</div>';
     }else{
-        // No match -> invalid url or account has already been activated.
-        echo '<div class="statusmsg">The url is either invalid or you already have activated your account.</div>';
+     
+        echo '<div class="statusmsg">error</div>';
     }
 
 }else{
-    // Invalid approach
-    echo '<div class="statusmsg">Invalid approach, please use the link that has been send to your email.</div>';
+
+    echo '<div class="statusmsg">error</div>';
 }
